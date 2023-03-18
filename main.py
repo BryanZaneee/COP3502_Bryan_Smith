9e4f2f210b750f0d45342823ce5cb348a3831107
Bryan Zane <bzane09@gmail.com>
2023-03-18 14:08:58 -0400

    Set up the main() and encode() functions, as well as the foundation to incorporating the decode function into option 2

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-print("gitman")
-print("commit and push man")
-print("verifying commit and push man")
+def main():
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit\n")
+        choice = input("Please enter an option: ")
+        if choice == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif choice == "2":
+            pass
+        elif choice == "3":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please choose again.")
+
+
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        new_digit = str((int(digit) + 3) % 10)
+        encoded_password += new_digit
+    return encoded_password
+
+
+if __name__ == '__main__':
+    main()
