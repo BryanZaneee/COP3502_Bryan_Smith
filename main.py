b493276578cef8223daaa8ed10f22f59abdfad48
Bryan Zane <bzane09@gmail.com>
2023-03-19 20:21:49 -0400

    Added comments to explain the code, and simplfied the encode function a little bit!

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Defining the menu and option selection
 def main():
     while True:
         print("Menu")
@@ -6,12 +7,16 @@ def main():
         print("2. Decode")
         print("3. Quit\n")
         choice = input("Please enter an option: ")
+        # Selection of choices for the user
+        # 1 makes use of the encode() function
         if choice == "1":
             password = input("Please enter your password to encode: ")
             encoded_password = encode(password)
             print("Your password has been encoded and stored!\n")
+        # 2 will be the decode function to undo the encoding
         elif choice == "2":
             pass
+        # Ends the program
         elif choice == "3":
             print("Goodbye!")
             break
@@ -20,10 +25,13 @@ def main():
 
 
 def encode(password):
+    # Stores "encoded_password" as an empty string variable
     encoded_password = ""
+    # Converts the password string into an integer.
+    # Each individual digit in the password will be added by 3, then % by 10.
+    # % by 10 ensures the new digit remains a single digit. ex * (8+3) % 10 = 1 *
     for digit in password:
-        new_digit = str((int(digit) + 3) % 10)
-        encoded_password += new_digit
+        encoded_password = str((int(digit) + 3) % 10)
     return encoded_password
 
 
